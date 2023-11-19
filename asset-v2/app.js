c36d54519e51f258de66a54fdda61e8d24e2fb89
PEFEBE <132106690+PEFEBE@users.noreply.github.com>
2023-11-19 11:16:29 -0300

    Update app.js

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -2,99 +2,83 @@
 var app = new function() {
     this.name = "Pop Zey", this.version = "Unreleased v2", this.date = "2024", this.folder = "asset-v2/", this.looptime = 8000, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#0b252b", this.col0 = "#a5d7c6", this.col1 = "#6dafa3", this.col2 = "#2f6c6a", this.col3 = "#235052", this.col4 = "#18373d", this.animearray = [{
         name: "01kick",
-        color: "af8553",
+        color: "17363c",
         uniqsnd: !0
     }, {
         name: "02snare",
-        color: "af8553",
+        color: "17363c",
         uniqsnd: !0
     }, {
         name: "03clic",
-        color: "af8553",
+        color: "17363c",
         uniqsnd: !0
     }, {
         name: "04klop",
-        color: "af8553",
+        color: "17363c",
         uniqsnd: !0
     }, {
         name: "05tombreak",
-        color: "af8553",
+        color: "17363c",
         uniqsnd: !0
     }, {
         name: "06bass",
-        color: "10a3b7",
+        color: "2e266d",
         uniqsnd: !0
     }, {
         name: "07mmmhah",
-        color: "10a3b7",
+        color: "2e266d",
         uniqsnd: !0
     }, {
         name: "08tututoung",
-        color: "10a3b7",
+        color: "2e266d",
         uniqsnd: !0
     }, {
         name: "09boowop",
-        color: "10a3b7",
+        color: "2e266d",
         uniqsnd: !0
     }, {
         name: "10woah",
-        color: "10a3b7",
+        color: "2e266d",
         uniqsnd: !0
     }, {
         name: "11nugununugu",
-        color: "fe0849",
+        color: "981e25",
         uniqsnd: !0
     }, {
         name: "12string",
-        color: "fe0849",
+        color: "981e25",
         uniqsnd: !0
     }, {
         name: "13lowarp",
-        color: "fe0849",
+        color: "981e25",
         uniqsnd: !1
     }, {
         name: "14settoarp",
-        color: "fe0849",
+        color: "981e25",
         uniqsnd: !0
     }, {
         name: "15duuduuduu",
-        color: "fe0849",
+        color: "981e25",
         uniqsnd: !0
     }, {
         name: "16somuch",
-        color: "9573fe",
+        color: "17191e",
         uniqsnd: !0
     }, {
         name: "17casting",
-        color: "9573fe",
+        color: "17191e",
         uniqsnd: !0
     }, {
         name: "18ahwoo",
-        color: "9573fe",
+        color: "17191e",
         uniqsnd: !0
     }, {
         name: "19lastday",
-        color: "9573fe",
+        color: "17191e",
         uniqsnd: !0
     }, {
         name: "20mind",
-        color: "9573fe",
-        uniqsnd: !0
-    }, {
-        name: "19lastday",
-        color: "9573fe",
-        uniqsnd: !0
-    }, {
-        name: "19lastday",
-        color: "9573fe",
-        uniqsnd: !0
-    }, {
-        name: "19lastday",
-        color: "9573fe",
-        uniqsnd: !0
-    }, {
-        name: "19lastday",
-        color: "9573fe",
+        color: "17191e",
         uniqsnd: !0
     }], this.bonusarray = [{
         name: "The Last Day",
